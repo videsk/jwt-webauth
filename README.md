c67f3f01478fda71b41a01588a476e0e4813334b
Matias <matiaslopezd@gmail.com>
2019-08-22 00:44:45 -0400

    NEW:Added pathname, hash, readme and future JWT HTTP check

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# front-auth-handler
- A Javascript frontend authorization key handler for works with JWT.
+# Front Authorization Handler
+ A Javascript frontend authorization key handler that works with JWT.
+ 
+ This library allows you do easy way to manage JWT in localStorage or sessionStorage.
+ 
+ ## How to use
+ For start you need instance `WebAuth` class.
+
+```js
+const auth = new WebAuth({ token: String, remember: Boolean, debug: Boolean });
+```
+
+After to instance the new manager need initialize.
+```js
+auth.init().then((result) => {
+    // Here do something
+    console.log(result);
+    // Output: { token: String, valid: Boolean, payload: Object, pathname: String, hash: String, search: Object }
+})
+.catch(() => {
+    // Is not valid
+})
+```
+
+### Check JWT when reload page
+For check when user reload app only you need is instance without token and `WebAuth` check automatically in localStorage and sessionStorage if exist valid JWT.
+
+```js
+const auth = new WebAuth({});
+auth.init().then((result) => {
+    // Exist valid JWT
+    // Ex: Redirect to result.pathname
+})
+.catch(() => {
+    /// Not exist valid JWT
+    //  Ex: Redirect to login or refresh access token
+});
+```
+
+## Key schema
+Every initialization returns a object with:
+
+* `valid`: Boolean indicate is valid or not
+* `token`: String is the token validated
+* `payload`: Object with payload of valid JWT
+* `pathname`: String with pathname when is initialized
+* `hash`: String with hash (Only if exist)
+* `search`: Object with search parameters (Only if exist)
+
+## Force to clean tokens
+If you need clean all token only need execute the follow code line
+```js
+auth.cleanTokens();
+```
--- src/index.js
+++ src/index.js
@@ -12,12 +12,13 @@
 import jwt_decode from 'jwt-decode';
 
 class WebAuth {
-    constructor({ key, jwt, debug, remember }) {
+    constructor({ key, jwt, debug, remember, checker }) {
         this.token = jwt; // JWT Token
         this.payload = null; // Payload of token
         this.key = key || 'auth-key'; // String of auth-key
         this.debug = debug || false; // By default is false
         this.remember = remember || false; // By default is false
+        this.checker = checker;
     }
 
     init() {
@@ -33,9 +34,15 @@ class WebAuth {
                         this.setPayload();
                         // Check the expiration of JWT
                         this.checkExpiration(valid => {
-                            const obj = { valid, token: this.token, payload: this.payload };
+                            // Get parameters
+                            const newObj = this.getSearchOrHash();
+                            // base obj
+                            const obj = { valid, token: this.token, payload: this.payload, pathname: this.getPathname() };
+                            // merge two obj
+                            Object.assign(obj, newObj);
+                            // validate
                             if (valid) resolve(obj); // Return valid JWT
-                            else this.Debug('warn', obj); // Return debug
+                            else reject(obj);
                         });
                 })
                 .catch(e => {
@@ -91,6 +98,72 @@ class WebAuth {
     cleanTokens() {
         window.localStorage.removeItem(this.key);
         window.sessionStorage.removeItem(this.key);
+    }
+
+    getPathname() {
+        return window.location.pathname;
+    }
+
+    getSearchOrHash() {
+        // Format hash, get search parameters and return object
+        const formatter = (value) => {
+            // nested formatter for search parameters
+            const searchFormatter = (parameters) => {
+                // return obj
+                const obj = {};
+                // add parameters like key and value
+                parameters.split('&').map(p => obj[p.split('=')[0]] = p.split('=')[1]);
+                // return obj
+                return obj
+            };
+            // object with all parameters of url
+            let obj = {};
+            // Get all parameters (split by ? for get all)
+            const parameters = value.split('?');
+            // map paramaters and add to the correct key
+            parameters.map(p => {
+                if (p.includes('#')) obj.hash = p.replace('#', '');
+                else if (p.includes('&')) obj.search = searchFormatter(p);
+            });
+            // return obj
+            return obj;
+        };
+        // Get search parameters
+        const search = window.location.search;
+        // Get hash
+        const hash = window.location.hash;
+        // return object based on url
+        return (search) ? formatter(search) : formatter(hash);
+    }
+
+    // Working for the next version 1.2.0
+    checkHTTP() {
+        return new Promise((resolve, reject) => {
+            if (typeof this.checker === 'object' && 'url' in this.checker && 'header') {
+                // Create a new header
+                const headers = new Headers();
+                // Map checker object
+                Object.keys(this.checker).map(key => {
+                    // Key to lower case
+                    const foo = key.toLowerCase();
+                    // check the key is not reserved
+                   if (foo !== 'authorization'
+                       && foo !== 'prefix'
+                       && foo !== 'body'
+                       && foo !== 'method') headers.append(key, this.checker.header[key]);
+                });
+                // Add to header object
+                headers.append('Authorization', `${this.checker.prefix} ${this.token}`);
+                // fetch to url
+                fetch(this.checker.url, { method: this.checker.method || 'POST', headers, body: this.checker.body || {} })
+                    .then(response => {
+                        // Return response
+                        resolve(response);
+                    })
+                    .catch(e => reject(e)); // Reject is not valid or something happen with server
+            }
+            else reject('Checker key not have the correct format or url/header keys not found');
+        });
     }
 
     Debug(type, message) {
