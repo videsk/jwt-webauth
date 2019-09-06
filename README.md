fb1505d2176bfedf96f9afd09082afea6eb4ccb3
Matias <matiaslopezd@gmail.com>
2019-09-06 19:05:53 -0400

    FIX:Added fetch function token

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
  
  This library allows you do easy way to manage JWT in localStorage or sessionStorage.
  
+**IMPORTANT: This library not validate JWT only check format and expiration time of token.**
+ 
 ## How to use
  For start you need instance `WebAuth` class.
 
@@ -37,6 +39,29 @@ auth.init().then((result) => {
 });
 ```
 
+### Validate JWT in backend
+This library not validate JWT only check expiration time and body of token. That mean is very ease change token with DevTools in sessionStorage or localStorage, for that reason we recommend set backend validation with this function.
+
+For validate in your backend only need set key `checker` like object with the follow properties:
+
+```js
+... = new WebAuth({
+    checker: {
+        url: String,
+        header: {
+            Origin: 'https://mydefault.com:80',
+            // Authorization is added automatically. Use prefix for set type of authorization
+        },
+        prefix: String, // By default is 'Bearer'
+        method: String, // By default is POST
+        body: Object || String, // Here add custom body
+    }
+});
+```
+
+### Expiration time
+When you initialize WebAuth, automatically create a expiration checker with a `setInterval` every **30 seconds**. If token expire will be executed ``
+
 ## Key schema
 Every initialization returns a object with:
 
@@ -46,6 +71,7 @@ Every initialization returns a object with:
 * `pathname`: String with pathname when is initialized
 * `hash`: String with hash (Only if exist)
 * `search`: Object with search parameters (Only if exist)
+* `checker`: Object with keys for server-side validation token
 
 ## Force to clean tokens
 If you need clean all token only need execute the follow code line
--- src/index.js
+++ src/index.js
@@ -4,45 +4,64 @@
 **  Class for manage authorization for Rest API
 **  Check automatically expiration of JWT Token
 **  Easy access to payload of JWT
+**  Automatically use of refresh token for get a new access token
 **  JWT decode dependency
 **
-**  Github: https://github.com/matiaslopezd/web-auth-manager - NPM: https://npmjs.com/package/@videsk/web-auth-manager
+**  Github: https://github.com/videsk/front-auth-handler - NPM: https://npmjs.com/package/@videsk/front-auth-handler
  */
 
 import jwt_decode from 'jwt-decode';
 
 class WebAuth {
-    constructor({ key, jwt, debug, remember, checker }) {
-        this.token = jwt; // JWT Token
-        this.payload = null; // Payload of token
-        this.key = key || 'auth-key'; // String of auth-key
-        this.debug = debug || false; // By default is false
-        this.remember = remember || false; // By default is false
-        this.checker = checker;
+
+    constructor({ key, tokens, debug, remember, config, expired }) {
+        this.tokens = {
+            access: (tokens && 'access' in tokens) ? tokens.access : null, // Set access token
+            refresh: (tokens && 'refresh' in tokens) ? tokens.refresh : null // Set refresh token
+        };
+        this.payloads = { access: null, refresh: null }; // Payload of access and refresh token
+        this.keys = {
+            access: (key && 'access' in key) ? key.access : 'auth-key', // Key of access token for save in local or session storage
+            refresh: (key && 'refresh' in key) ? key.refresh : 'auth-key-refresh' // Key of refresh token for save in local or session storage
+        };
+        this.debug = (typeof debug === 'boolean') ? debug : false; // By default is false
+        this.remember = (typeof remember === 'boolean') ? remember : false; // By default is false
+        this.config = (typeof config === 'object') ? config : null; // Parameters for validate and get new token
+        this.expired = (typeof expired === 'function') ? expired : null; // Handler for positive or negative result of validation
+        this.interval = null;
     }
 
     init() {
         return new Promise((resolve, reject) => {
-            // Check if exist key in localStorage or sessionStorage
-            const cToken = window[this.checkStorage()].getItem(this.key);
-            // Save storage token or new token
-            this.token = this.token || cToken;
+            // Save access token
+            this.token = this.token || this.getFromStorage(this.keys.access);
+            // Save refresh token
+            this.refresh = this.refresh || this.getFromStorage(this.keys.refresh);
             // Refresh or add token
             this.setup()
                 .then(() => {
-                        // Set payload of JWT
-                        this.setPayload();
-                        // Check the expiration of JWT
-                        this.checkExpiration(valid => {
+                        // Set payload of access token
+                        this.setPayload('access');
+                        // Set payload of refresh token
+                        if (this.tokens.access) this.setPayload('refresh');
+                        /*
+                        **  Here check the access token
+                        **  if is expired try to use refresh token for get a new
+                        **  in other case reject and execute return catch
+                         */
+                        this.checkExpiration('access',valid => {
                             // Get parameters
-                            const newObj = this.getSearchOrHash();
+                            const pathnameObject = { pathname: Object.assign(this.getSearchOrHash(), this.nestedPathname()) };
                             // base obj
-                            const obj = { valid, token: this.token, payload: this.payload, pathname: this.getPathname() };
+                            const finalObj = { valid, tokens: this.tokens, payloads: this.payloads };
                             // merge two obj
-                            Object.assign(obj, newObj);
-                            // validate
-                            if (valid) resolve(obj); // Return valid JWT
-                            else reject(obj);
+                            Object.assign(finalObj, pathnameObject);
+                            // Add checker for expiration time of JWT
+                            this.createChecker();
+                            // Check in backend
+                            this.checkHTTP()
+                                .then(() => resolve(finalObj)) // Return valid JWT
+                                .catch(() => reject(finalObj));
                         });
                 })
                 .catch(e => {
@@ -59,49 +78,76 @@ class WebAuth {
             // Remove all others JWT storage
             this.cleanTokens();
             // Refresh or save in dynamic route
-            if (!this.token) reject('Token is not defined');
+            if (!this.tokens.access) reject('Token is not defined');
             else {
-                window[route].setItem(this.key, this.token);
+                // Save access token
+                window[route].setItem(this.keys.access, this.tokens.access);
+                // Save refresh token
+                if (this.tokens.refresh) window[route].setItem(this.keys.refresh, this.tokens.refresh);
+                // Finish
                 resolve();
             }
         });
     }
 
-    checkExpiration(callback) {
+    checkExpiration(token, callback) {
         // Set expiration time of JWT in local time
-        const expiration = this.payload.exp*1000;
+        const expiration = this.payloads[token].exp*1000;
         // Get local time of user
         const today = new Date().getTime();
         // Check if the expiration date is greater of today
         if (expiration >= today) callback(true); else callback(false);
     }
 
-    setPayload() {
+    setPayload(token) {
         // Error if payload isn't a object
         const error = () => {
-            this.Debug('error', { token: this.token, payload: this.payload });
+            this.Debug('error', { token: this.tokens[token], payload: this.payloads[token] });
             this.Debug('error', new Error('Payload isn\'t an object'));
             return {};
         };
         // Decode JWT and get-set payload
-        const cPayload = jwt_decode(this.token);
+        const payload = jwt_decode(this.tokens[token]);
         // Debug
-        this.Debug('info', cPayload);
+        this.Debug('info', payload);
         // Set payload
-        this.payload = (typeof cPayload === 'object') ? cPayload : error();
+        this.payloads[token] = (typeof payload === 'object') ? payload : error();
     }
 
-    checkStorage() {
-        return (window.localStorage.getItem(this.key)) ? 'localStorage' : 'sessionStorage';
+    checkStorage(key) {
+        return (window.localStorage.getItem(key)) ? 'localStorage' : 'sessionStorage'; // Get the store of token/refresh token
+    }
+
+    getFromStorage(key) {
+        return window[this.checkStorage(key)].getItem(key);
     }
 
     cleanTokens() {
+        // Remove from localStorage
         window.localStorage.removeItem(this.key);
+        window.localStorage.removeItem(this.refreshKey);
+        // Remove from sessionStorage
         window.sessionStorage.removeItem(this.key);
+        window.sessionStorage.removeItem(this.refreshKey);
     }
 
     getPathname() {
-        return window.location.pathname;
+        return window.location.pathname; // Get the pathname
+    }
+
+    nestedPathname() {
+        // Get the current pathname
+        let pathname = this.getPathname();
+        // Remove the first element in array is slash
+        pathname = pathname.split('/');
+        // Remove the first empty element
+        pathname.shift();
+        // Create array with path
+        let array = [];
+        // Push elements to array
+        pathname.map((path, index) => array.push({ path: pathname[index], level: index }));
+        // Return array with list
+        return { plain: this.getPathname(), byLevels: array };
     }
 
     getSearchOrHash() {
@@ -120,7 +166,7 @@ class WebAuth {
             let obj = {};
             // Get all parameters (split by ? for get all)
             const parameters = value.split('?');
-            // map paramaters and add to the correct key
+            // map parameters and add to the correct key
             parameters.map(p => {
                 if (p.includes('#')) obj.hash = p.replace('#', '');
                 else if (p.includes('&')) obj.search = searchFormatter(p);
@@ -137,33 +183,56 @@ class WebAuth {
     }
 
     // Working for the next version 1.2.0
-    checkHTTP() {
+    checkHTTP(urlKey = 'check') {
         return new Promise((resolve, reject) => {
-            if (typeof this.checker === 'object' && 'url' in this.checker && 'header') {
+            if (this.config
+                && typeof this.config === 'object' // Validate is object
+                && 'url' in this.config
+                && typeof this.config.url === 'object') {
                 // Create a new header
-                const headers = new Headers();
-                // Map checker object
-                Object.keys(this.checker).map(key => {
+                const header = new Headers();
+                // Destructing object and get keys
+                const { headers, url, prefix, method, body } = this.config;
+                // Map checker object only if exist
+                if (headers) Object.keys(headers).map(key => {
                     // Key to lower case
                     const foo = key.toLowerCase();
                     // check the key is not reserved
-                   if (foo !== 'authorization'
-                       && foo !== 'prefix'
-                       && foo !== 'body'
-                       && foo !== 'method') headers.append(key, this.checker.header[key]);
+                    if (foo !== 'authorization') header.append(key, headers[key]);
                 });
                 // Add to header object
-                headers.append('Authorization', `${this.checker.prefix} ${this.token}`);
+                header.append('Authorization', `${prefix || 'Bearer'} ${this.token}`);
                 // fetch to url
-                fetch(this.checker.url, { method: this.checker.method || 'POST', headers, body: this.checker.body || {} })
+                fetch(url[urlKey], { method: method || 'POST', header, body: body || {} })
                     .then(response => {
                         // Return response
                         resolve(response);
                     })
-                    .catch(e => reject(e)); // Reject is not valid or something happen with server
+                    .catch(e => reject(e)); // Reject is not valid or something happen with server/url
+            } else {
+                // Check if checker is declared like object or not backend validation
+                if (typeof this.config === 'undefined') resolve(); // Not backend validation is required
+                else reject('Config dont\'t have the correct format or url key not found.'); // Some declarations are not correct
             }
-            else reject('Checker key not have the correct format or url/header keys not found');
         });
+    }
+
+    getNewToken() {
+        // Get new token if the access token expired
+    }
+
+    createChecker(token) {
+        // Handler of JWT expiration
+        const handler = () => {
+            if (new Date().getTime() > this.payloads[token].exp*1000) {
+                // Clear interval
+                clearInterval(this.interval);
+                // Execute custom expired function
+                this.expired();
+            }
+        };
+        // Create interval
+        this.interval = window.setInterval(handler, 1000);
     }
 
     Debug(type, message) {
